55df215b5dddd6113a12f1761e5b7d571cbb15d4
Daren Lee <dlee@weatherstream.com>
2025-10-31 16:37:10 -0600

    src: snailboard_microblaze_demo: Added microblaze motor control demo

---
+++ src/snailboard_microblaze_demo/snail_microblaze_app/Debug/src/main.d
@@ -0,0 +1,46 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/sleep.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_types.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_io.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_printf.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/bspconfig.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/mb_interface.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_assert.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_printf.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xintc.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xstatus.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xintc_l.h
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/sleep.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_types.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_io.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_printf.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/bspconfig.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/mb_interface.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_assert.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_printf.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xintc.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xstatus.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xintc_l.h:
---
+++ src/snailboard_microblaze_demo/snail_microblaze_app/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_cache.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/mb_interface.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_types.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_assert.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h \
+ ../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_cache.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/mb_interface.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_types.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_assert.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xil_exception.h:
+
+../../snail_microblaze_app_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
